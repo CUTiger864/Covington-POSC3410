89df9b7c02da00e2115bf8e12ad7e7da0aa0fed8
CUTiger864 <77397179+CUTiger864@users.noreply.github.com>
2021-01-19 12:05:12 -0500

    First Commit-DAL 1

---
+++ Covington-DAL1.R
@@ -0,0 +1,33 @@
+# Title: #### #John Covington: #January 14, 2021
+# Let's Try Some Basic Arithmetic
+##### Sum of 1 and 11+1
+# 13
+# Divide 365 by 12365/12
+# 0.0024599003
+# Your turn, multiply 10 by 12
+# 120
+# Your turn, add 28 + 3828+38
+# 3894
+# Order of Operations in R1+1*(365/12)
+# 6*6= 36
+# 9/3.142= 2.8644175684
+# Learning to assign variables 
+##### myFirstVar<- "Hello world!"
+# mySecondVar<-16# 
+# "Clemson" to`home`home <- "Clemson"
+# "Tigers" to`h_mascot`h_mascot <- "Tigers"
+# "Ohio State" to`away3
+# 'Buckeyes'to`a_mascot`
+# Assign 31 to``homeScore <- 31
+# Assign 0 to`awayScore`
+# Assign TRUE to homeWinhomeWin<- TRUE
+# Assign FALSE to awayWin
+
+# set up ####
+# Libraries
+
+# Data
+
+# Script ####
+
+Colleges
